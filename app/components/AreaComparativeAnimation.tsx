3c4d57594e1320b85be90614657446afeaeb6ec8
Wilton Ramos <wrs@cin.ufpe.br>
2025-03-27 19:52:32 -0300

    Create components for Lottie animations

---
+++ app/components/AreaComparativeAnimation.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import Lottie from "lottie-react";
+import areaComparative from "./areaComparativeLottieData.json";
+
+const AreaComparativeAnimation = () => <Lottie animationData={areaComparative} loop={true}/>;
+
+export default AreaComparativeAnimation;
---
+++ app/components/CarbonSequestrationBreathingComparative.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import Lottie from "lottie-react";
+import breatingComparativeAnimation from "./carbonSequestrationBreathingComparativeLottieData.json";
+
+const CarbonSequestrationBreathingComparative = () => <Lottie animationData={breatingComparativeAnimation} loop={true}/>;
+
+export default CarbonSequestrationBreathingComparative;
