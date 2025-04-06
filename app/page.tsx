fc0c08f3291d816d899d9d20ee6a73fb5d4a1a25
Wilton Ramos <wrs@cin.ufpe.br>
2025-04-06 14:57:07 -0300

    Adds video

--- app/page.tsx
+++ app/page.tsx
@@ -47,283 +47,45 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Background fixo para toda a página */}
-      <div className="fixed inset-0 -z-50">
-        <Image
-          src={backgroundImg}
-          alt="Background da seção"
-          fill
-          style={{ objectFit: "cover", objectPosition: "center" }}
-        />
-        <div className="absolute inset-0 bg-black/50" />
-      </div>
-
       <main className="relative">
         {/* Primeira Seção */}
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
+        <section className="w-full min-h-screen flex flex-col text-white">
+          <div className="max-w-4xl mx-auto flex flex-col min-h-screen space-y-8 mx-16 bg-black/60">
+            <p></p>
+            <p>
+              Por anos, um bioma representado na grande mídia pela morte e pela seca: a caatinga, que é o único bioma exclusivamente brasileiro, na verdade exala mais vida do que se pode ver a olho nu. As variações de solo se transformam em diferentes tipos de paisagens, como serras, chapadas e afloramento de rochas chamados de ‘‘lajedos’’.
+            </p>
+            <p>
+              Essa variedade se estende para a fauna e a flora, com cerca de 1.794 espécies de animais vertebrados e invertebrados, e mais de 3 mil espécies de plantas vasculares. Com um clima quente e seco, onde longos períodos de estiagem são a regra, essa paisagem abriga milhões de pessoas que dependem dela para viver.
+            </p>
 
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  O Bioma nordestino e pernambucano
-                </h2>
-              </div>
-            </div>
+            <video
+              className="rounded-lg object-cover"
+              controls
+              muted
+            >
+              <source src="/agroflorestas.mp4" type="video/mp4" />
+              Seu navegador não suporta vídeo HTML5.
+            </video>
 
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  A Caatinga é um bioma exclusivamente brasileiro e ocupa a
-                  maior área do estado de Pernambuco, cerca de 83% do
-                  território, abrangendo regiões de clima árido e semiárido. Ela
-                  tem um clima semiárido, ou seja, quente e seco, com longas
-                  estiagens. É um bioma extremamente diverso, onde o solo se
-                  transforma em diferentes tipos de paisagens, com serras,
-                  morros e extensões rochosas chamadas de “lajedos”. Essa
-                  variedade se estende para a fauna e a flora, com cerca de
-                  1.794 espécies de invertebrados, e 3150 espécies de plantas
-                  vasculares.
+            <div className="flex flex-col md:flex-row gap-8">
+              <div className="flex-1 aspect-square space-y-8">
+                <p>
+                  A caatinga cobre 83% do território de Pernambuco, dominando o sertão e parte do agreste, o que dá cerca de 135 municípios. O bioma se estende por outros oito estados do Nordeste e parte do Sudeste, abrangendo Alagoas, Bahia, Ceará, Maranhão, Paraíba, Piauí, Rio Grande do Norte, Sergipe e uma pequena porção de Minas Gerais.
                 </p>
-                <br />
-                <p className="text-base md:text-lg leading-relaxed">
-                  Apenas em Pernambuco, a Caatinga abrange cerca de 135
-                  municípios, envolvendo inúmeras famílias que muitas vezes
-                  vivem da agricultura, atividade ameaçada pela crescente
-                  degradação do bioma.
+                <p>
+                  E esse ecossistema ganha ainda mais importância devido ao seu potencial no sequestro de carbono. Mais de uma década de estudos do Observatório Nacional da Caatinga apontaram que esse é o bioma brasileiro que mais se destaca pelo alto desempenho no sequestro de carbono (CO²).
+                </p>
+                <p>
+                  O excesso de CO² na atmosfera é a principal causa do efeito estufa, relacionado ao aquecimento global. Na caatinga, para cada 100 toneladas de CO² capturadas, entre 45% e 60% permanecem retidas e não retornam à atmosfera. 
                 </p>
               </div>
+              <div className="flex-1 aspect-square bg-gray-300 rounded-lg" />
             </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  Desertificação da Caatinga
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  A desertificação é um processo em que a vegetação de áreas
-                  secas diminui a ponto de desaparecer, o solo sofre tamanha
-                  degradação a ponto de se tornar excessivamente seco e sem
-                  nutrientes, resultando em uma paisagem similar a dos desertos.
-                  É um fenômeno antrópico, isto é, inteiramente causado pela
-                  interferência humana, que também é o caso da Caatinga.{" "}
-                </p>
-                <br />
-                <p className="text-base md:text-lg leading-relaxed">
-                  O processo de desertificação da Caatinga não é algo recente,
-                  nos anos de 1970 o agrônomo João Vasconcelos Sobrinho já tinha
-                  sido responsável pelos primeiros estudos sobre este crescente
-                  problema no bioma. Trata-se de uma área com um índice muito
-                  grande de desmatamento, sem nenhum movimento político para
-                  frear a situação. A geógrafa Jocimara Lobão, pesquisadora do
-                  MapBiomas, explica em entrevista para o Brasil de Fato: “A
-                  Caatinga tem uma dinâmica de processos mais lentos.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  Desertificação da Caatinga
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  Uma degradação que é feita na Caatinga leva muito mais tempo
-                  para recuperar que em outro ambiente. Quando a gente fala em
-                  degradação e desmatamento, a gente está falando em acelerar
-                  processos que vão gerar a desertificação”. Um estudo publicado
-                  em 2024 pela revista Applied Soil Ecology já constatou que a
-                  desertificação no bioma reduz mais de 50% a funcionalidade do
-                  solo. Esse fator reduz consideravelmente a capacidade de
-                  sustentar o crescimento das plantas, afetando diretamente a
-                  vida humana e animal no local. Esse grande estado de
-                  degradação contribui ainda mais para o agravamento da
-                  condição, o solo enfraquecido dessa maneira impede parte da
-                  infiltração de água, o que acelera o processo de erosão.
-                  Devido ao desmatamento desenfreado, a Caatinga já perdeu mais
-                  de 40% de seu território original, segundo dados do Relatório
-                  Anual do Desmatamento de 2023 do MapBiomas, que também relatou
-                  um aumento de 43,3% de áreas desmatadas em comparação ao ano
-                  anterior, 2022.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  Desertificação da Caatinga
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  Na época do lançamento do relatório, Washington Rocha,
-                  coordenador da equipe da Caatinga do MapBiomas explicou causas
-                  desse aumento: “O maior desmatamento verificado na Caatinga
-                  foi impulsionado pela expansão de atividades agropecuárias. Um
-                  fenômeno que capturamos é o desmatamento para fins de
-                  implantação de parques solares e eólicos crescendo pelo
-                  bioma”. Mais 4.302 hectares foram desmatados por
-                  empreendimentos de energia renováveis (eólica e solar), de
-                  acordo com a revista Exame. No meio desta situação, secas
-                  ficam cada vez mais severas e a expansão do agronegócio
-                  impulsiona a degradação na área, afetando não apenas as
-                  famílias residentes mas também a vegetação e os animais.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  A caatinga é o bioma brasileiro com maior potencial de
-                  sequestro de carbono
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  Diferente do que era pensado antigamente, a Caatinga, bioma
-                  exclusivo do Brasil, é um bioma muito rico, com propriedades
-                  surpreendentes que o tornam a vegetação número um no sequestro
-                  de carbono. Com cerca de 860 Km², a vegetação da caatinga é
-                  seca mas repleta de vida, abarcando mais de 3.300 espécies e
-                  contribuindo com a redução dos gases do efeito estufa. “A
-                  floresta presta muitos benefícios, incluindo a mitigação, no
-                  caso o sequestro de carbono, a retirada de CO2 da atmosfera
-                  que está ligado ao aquecimento global”, pontuou John Cunha,
-                  pesquisador do Observatório da Caatinga. Estudos do
-                  Observatório Nacional da Caatinga apontam que a Caatinga,
-                  mesmo durante duras secas, é excelente no sequestro de CO2,
-                  também conhecido como Dióxido de Carbono. Essa substância
-                  gasosa existe em abundância na atmosfera, no entanto, ações
-                  humanas, como queima de carvão e petróleo, expelem ainda mais,
-                  agravando o efeito estufa e aquecendo ainda mais o planeta. Em
-                  áreas mais úmidas, conhecidas como Caatinga Hipoxerófilas, a
-                  vegetação sequestra até três vezes mais carbono por ano, em
-                  comparação com regiões mais secas.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  A caatinga é o bioma brasileiro com maior potencial de
-                  sequestro de carbono
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  A caatinga também se mostra super eficiente na utilização e
-                  armazenamento do carbono, apresentando uma média de 45% em
-                  eficiência. Por exemplo, a cada 100 toneladas de CO2
-                  capturadas pela caatinga, 45 ficam retidas. Além disso, cerca
-                  de 3,5 quilogramas de dióxido de carbono são fixadas por metro
-                  cúbico de água transpirada pelo bioma. Apesar de ser um dos
-                  mais importantes biomas, a caatinga sofre com uma forte
-                  degradação, empobrecendo o solo e levando todo um ecossistema
-                  à morte. De acordo com a Agência Brasil, o país já perdeu 34
-                  milhões de hectares dos 82,6 milhões de hectares da Caatinga,
-                  além de ter 60% da vegetação nativa ocupada. Do contrário do
-                  que era imaginado, esse processo de desertificação não
-                  acontece somente pelas mãos do pequeno agricultor, mas
-                  principalmente pelas grandes empresas.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="w-full min-h-screen flex flex-col md:flex-row text-white">
-          <div className="max-w-7xl mx-auto flex flex-col md:flex-row min-h-screen bg-black/60">
-            {/* Coluna Esquerda */}
-            <div className="w-full md:w-1/2 max-auto px-4 flex flex-col md:flex-row min-h-screen text-white bg-black">
-              <div className="relative w-full md:w-2/3 flex flex-col justify-between p-8">
-                {/* Jitter */}
-
-                <h2 className="text-2xl md:text-4xl font-bold self-start mt-auto">
-                  A caatinga é o bioma brasileiro com maior potencial de
-                  sequestro de carbono
-                </h2>
-              </div>
-            </div>
-
-            {/* Coluna Direita (Texto) */}
-            <div className="w-full md:w-1/2 p-8 flex items-center">
-              <div>
-                <p className="text-base md:text-lg leading-relaxed">
-                  “A gente tem visto novas dinâmicas de degradação impulsionadas
-                  pelos mega empreendimentos. Esses que têm capacidade de fato
-                  para modificar o ambiente de forma significativa”, pontuou o
-                  pesquisador. Além disso, De acordo com John Cunha, algumas
-                  questões têm sido pontuais nesse processo de modificação da
-                  Caatinga:
-                </p>
-                <br />
-                <p className="text-base md:text-lg leading-relaxed">
-                  O pesquisador também reforçou que a degradação da caatinga não
-                  está somente ligada à escassez de água, por isso, a criação e
-                  oferta de Oásis não deve ser vista como única solução para a
-                  desertificação do ambiente.
-                </p>
-              </div>
-            </div>
+          
+            <p>
+              Mas a caatinga não está livre da degradação, assim como outros biomas do Brasil. De acordo com o Ibama, o país já perdeu 34 milhões dos 82,6 milhões de hectares do bioma. Ao todo, cerca de 27 milhões de pessoas vivem na região e o avanço da degradação ameaça não só a biodiversidade, mas as famílias que tiram da terra seu sustento, o que torna urgente a busca por soluções sustentáveis. 
+            </p>
           </div>
         </section>
       </main>
