d21d992dfc59643e2b329e7483ef8d005b9ffa21
Pedro H Silva <phos@cin.ufpe.br>
2025-04-10 09:00:54 -0300

    Implementa carrossel

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          // Aqui definimos a chave "roboto" que poderá ser usada via "font-roboto" nas classes do Tailwind
+          unbounded: ['Unbounded', 'normal'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
